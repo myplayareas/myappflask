9337c8d2bf961545e44e2a16e754b0fe167d6376
armandossrecife <armando.sousa@gmail.com>
2021-10-13 00:33:57 -0300

    Aplicação atualizada para gerenciar repositórios dos usuários.

--- dao.py
+++ dao.py
@@ -1,10 +1,24 @@
-class Usuario: 
-    def __init__(self, id, username, name, email, password):
+class Usuario:
+    repositorios = []
+    def __init__(self, id, name, username, email, password):
         self.id = id
-        self.username = username
         self.name = name
+        self.username = username
         self.email = email
         self.password = password
+
+    def set_repositorios(self, repositorios):
+        self.repositorios = repositorios
+
+class Repositorio:
+    def __init__(self, id, name, link, creation_date, analysis_date, analysed, user_id):
+        self.id = id
+        self.name = name
+        self.link = link
+        self.creation_date = creation_date
+        self.analysis_date = analysis_date
+        self.analysed = analysed
+        self.user_id = user_id
 
 class RepositorioUsuarios:
     def __init__(self, lista):
@@ -28,6 +42,39 @@ class RepositorioUsuarios:
                 usuario = each
                 break
         return usuario
-
+    
     def lista_todos_os_usuarios(self):
         return self.lista
+
+class RepositorioRepositorios:
+    def __init__(self, lista):
+        self.lista = lista
+
+    def insere_repositorio(self, repositorio):
+        self.lista.append(repositorio)
+
+    def consulta_repositorio_por_nome(self, nome):
+        repositorio = None
+        for each in self.lista:
+            if each.nome == nome:
+                repositorio = each
+                break
+        return repositorio
+
+    def consulta_repositorio_por_id(self, id):
+        repositorio = None
+        for each in self.lista:
+            if each.id == id:
+                repositorio = each
+                break
+        return repositorio
+    
+    def lista_todos_os_repositorios(self):
+        return self.lista
+
+    def consulta_repositorios_por_id_usuario(self, id_usuario):
+        lista_reposigorios_usuario = []
+        for each in self.lista:
+            if each.user_id == id_usuario:
+                lista_reposigorios_usuario.append(each)
+        return lista_reposigorios_usuario
--- main.py
+++ main.py
@@ -1,62 +1,59 @@
 from flask import Flask
-from flask import render_template, request, url_for, redirect, flash, session
-
+from flask import render_template, request, redirect, url_for, flash, session
 import datetime
 from . import dao
 
 app = Flask(__name__)
-app.secret_key = "chavedaminhaaplicacao"
+app.secret_key = 'chavedaaplicacao'
 
 repositorioUsuarios = dao.RepositorioUsuarios([])
+repositorioRepositorios = dao.RepositorioRepositorios([])
 
 @app.route('/')
 def index_page():
     now = datetime.datetime.now()
-    now = now.strftime("%d/%m/%y %H:%M:%S")
-    my_content = f'Hora do sevidor {now}'
+    return render_template('index.html', mensagens=now)
 
-    return render_template('index.html', my_message=my_content)
-
+# Dashboard do usuario logado
 @app.route('/home')
 def home_page():
-    if session['user_id'] is None:
-        return redirect(url_for("login_page"))
+    usuario_logado = repositorioUsuarios.consulta_usuario_por_id(session['user_id'])
+    if usuario_logado is None:
+        return redirect(url_for("logout"))
 
-    usuario_logado = repositorioUsuarios.consulta_usuario_por_id(session["user_id"])
-    
-    return render_template('home.html', usuarios=repositorioUsuarios.lista_todos_os_usuarios(), usuario_logado=usuario_logado)
+    repositorios_do_usuario = repositorioRepositorios.consulta_repositorios_por_id_usuario(session['user_id'])
+
+    return render_template('home.html', repositorios=repositorios_do_usuario, usuario_logado=usuario_logado)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        confirma_password = request.form["confirma_password"]
-        name = request.form["name"]
-        email = request.form["email"]
+    if request.method == 'POST':
+        username = request.form['username']
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        confirma_password = request.form['confirma_password']
 
-        id_usuario = len(repositorioUsuarios.lista_todos_os_usuarios()) + 1
-        usuario = dao.Usuario(id=id_usuario, username=username, name=name, email=email, password=password)
+        if repositorioUsuarios.consulta_usuario_por_nome(username) is None:
+            id_usuario = len(repositorioUsuarios.lista) + 1
+            usuario = dao.Usuario(id_usuario, name, username, email, password)
+            repositorioUsuarios.insere_usuario(usuario)
+            flash(f'Usuario {usuario.username} inserido com sucesso!', category='success')
+            return redirect(url_for('login_page'))
 
-        # Checa se o usuário já existe no sistema, caso não exista cria um novo usário
-        # se o usuário não existe insere na lista e redireciona para a página de login
-        if (repositorioUsuarios.consulta_usuario_por_nome(username) is None):  
-            repositorioUsuarios.insere_usuario(usuario)      
-            return redirect(url_for("login_page"))
-        else:
-            flash(f"User {username} is already registered.")
-        
-    return render_template("register.html")
+        flash('Usuário já existe!', category='danger')
+
+    return render_template('register.html')
 
 def valida_login(username, password):
     usuario = repositorioUsuarios.consulta_usuario_por_nome(username)
     if usuario is None:
         return False
-    else:
+    else: 
         if (usuario.password == password):
             return True
-
-    return False
+        else: 
+            return False
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
@@ -64,18 +61,55 @@ def login_page():
         username = request.form['username']
         password = request.form['password']
 
-        if valida_login(username, password):
+        if valida_login(username, password):        
             usuario = repositorioUsuarios.consulta_usuario_por_nome(username)
-            session["user_id"] = usuario.id
-            # carrega a página principal da aplicacao
+            session['user_id'] = usuario.id
+            flash(f'Usuário {usuario.username} logado com sucesso!', category='success')
+            print(f'Name: {usuario.name}, Username: {usuario.username}, logou as: {datetime.datetime.now()}')
             return redirect(url_for('home_page'))
-        else:
-            flash('Invalid username/password')
+        # se as informacoes forem invalidas devolve mensagem de erro.
+        flash('Usuário ou senha inválida!', category='danger')
 
     return render_template('login.html')
 
 @app.route('/logout')
-def logout_page():
-    if session is not None:
-        session.clear()
-    return redirect(url_for("login_page"))
+def logout():
+    usuario = repositorioUsuarios.consulta_usuario_por_id(session['user_id'])
+    if usuario is not None: 
+        if session is not None:
+            session.clear()
+            flash(f'Usuário {usuario.username} deslogado com sucesso!', category='success')
+
+    return redirect(url_for('login_page'))
+
+def repositorio_ja_existe(name, link, lista):
+    checa = False
+    if len(lista) > 0:
+        for each in lista:
+            if each.name == name or each.link == link:
+                checa = True
+    return checa
+
+@app.route('/repository', methods=['GET', 'POST'])
+def repository_page():
+    if request.method == 'POST':
+        name = request.form['name']
+        link = request.form['link']
+
+        lista_repositorios_do_usuario = repositorioRepositorios.consulta_repositorios_por_id_usuario(session['user_id'])
+        # se o repositorio ainda não existe na lista de repositorios do usuario logado insere na lista de repositorios do usuario logado
+        if not repositorio_ja_existe(name, link, lista_repositorios_do_usuario):
+            id_repositorio = len(repositorioRepositorios.lista) + 1
+            repositorio = dao.Repositorio(id=id_repositorio, 
+                                            name=name, link=link, creation_date=datetime.datetime.now(), 
+                                            analysis_date=None, analysed=0, user_id=session['user_id'])
+            
+            repositorioRepositorios.insere_repositorio(repositorio)
+
+            flash(f'Repositório {repositorio.name} inserido com sucesso!', category='success')
+
+            return redirect(url_for('home_page'))
+
+        flash('Repositório já existe!', category='danger')
+
+    return render_template('repository.html')
