24eb26b8c90317ff5e01be4dd78b96a205aa2bfa
armandossrecife <armando.sousa@gmail.com>
2021-10-19 21:54:12 -0300

    Criada a feature para automatizar a criação do banco.

--- sysrepository/__init__.py
+++ sysrepository/__init__.py
@@ -4,7 +4,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sysrepository.db'
+banco_de_dados = 'sysrepository.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + banco_de_dados
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 
 db = SQLAlchemy(app)
