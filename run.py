8af31d855414b0d920fcbc5b302063a6ed57ecf3
Armando Soares Sousa <armando.sousa@gmail.com>
2021-10-19 21:12:41 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 from sysrepository import app
+from sysrepository import db, banco_de_dados
+from sysrepository.dao import Usuario, Repositorio
 
-if __name__ == '__main__': 
+CRIA_BANCO_VAZIO = False
+
+try:
+    if CRIA_BANCO_VAZIO:
+        # Cria um banco vazio
+        db.drop_all()
+        db.create_all()
+        print(f'Banco {banco_de_dados} criado com sucesso!')
+except Exception as e:
+    print(f'Erro ao criar o Banco {banco_de_dados} - {e}')
+
+if __name__ == '__main__':
     app.run(debug=True)
